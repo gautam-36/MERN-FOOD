67a63d5e853ab1fa7942fd84bbcac61c240b9328
gautam-36 <gautamkumarg660@gmail.con>
2024-06-04 17:56:56 +0530

    stripe added

---
+++ frontend/src/Success.js
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div className='bg-green-200 w-full max-w-md m-auto h-36 flex justify-center items-center font-semibold text-lg'>
+        <p>Payment is Successfully</p>
+    </div>
+  )
+}
+
+=======
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div className='bg-green-200 w-full max-w-md m-auto h-36 flex justify-center items-center font-semibold text-lg'>
+        <p>Payment is Successfully</p>
+    </div>
+  )
+}
+
+>>>>>>> e6e82dc8f6524e4c16e3aa266baecb4a77dee8d7
+export default Success
